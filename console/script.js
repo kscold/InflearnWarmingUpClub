6653f80f8d292521a8b79731ec6bc974139b8105
kscold <chan6502@gmail.com>
2024-04-25 23:51:43 +0900

    #1 - prettier 설정 및 console 객체에 대한 실습
    prettier 설정 및 여러 console 객체에 대한 실습을 진행함

---
+++ console/script.js
@@ -0,0 +1,22 @@
+console.log('안녕하세요');
+console.log(123);
+console.log(true);
+
+var greeting = '안녕하세요';
+console.log(greeting);
+console.log({ a: 'a', b: 'b' });
+console.table({ a: 'a', b: 'b' }); // 테이블로 보게 만들 수 있음
+
+console.error('Error!'); // 에러 메세지를 보여줌
+console.warn('Warning!'); // 경고 메세지를 보여줌
+
+console.time('Hello');
+
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.timeEnd('Hello');
+
+//  console.clear(); // 모든 콘솔을 지우는 것
